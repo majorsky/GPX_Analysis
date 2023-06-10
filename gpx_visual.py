04a3a62a03fc2dca1893bf3fc88836736a7b969f
Sergey Gerasimov <39904501+majorsky@users.noreply.github.com>
2023-06-10 11:49:04 +0000

    Update gpx_visual.py

--- gpx_visual.py
+++ gpx_visual.py
@@ -14,8 +14,9 @@ def visualize_route(points):
     lats = [point.latitude for point in points]
     lngs = [point.longitude for point in points]
 
-    # Создание объекта карты
+    # Создание объекта карты спутникового типа
     gmap = gmplot.GoogleMapPlotter(lats[0], lngs[0], 14)
+    gmap.map_type = "satellite"  # Установка типа карты на спутниковый
 
     # Добавление маркера на начальную точку
     gmap.marker(lats[0], lngs[0], 'cornflowerblue')
@@ -33,8 +34,8 @@ def plot_speed_graph(points):
 
     # Расчет расстояния и скорости между точками
     for i in range(1, len(points)):
-        distance = points[i].distance_2d(points[i-1])
-        speed = distance / (points[i].time - points[i-1].time).total_seconds()
+        distance = points[i].distance_2d(points[i-1]) / 1000  # перевод в километры
+        speed = distance / (points[i].time - points[i-1].time).total_seconds() * 3600  # перевод в км/ч
 
         distances.append(distance)
         speeds.append(speed)
@@ -47,17 +48,22 @@ def plot_speed_graph(points):
         cumulative_distances.append(cumulative_distances[-1] + distances[i])
         cumulative_speeds.append(speeds[i])
 
+    # Расчет общей средней скорости
+    total_distance = cumulative_distances[-1]
+    total_time = points[-1].time - points[0].time
+    average_speed = total_distance / total_time.total_seconds() * 3600  # перевод в км/ч
+
     # Построение графика скорости
     plt.plot(cumulative_distances, cumulative_speeds)
-    plt.xlabel('Distance (m)')
-    plt.ylabel('Speed (m/s)')
-    plt.title('Speed vs Distance')
+    plt.xlabel('Distance (km)')  # изменено на километры
+    plt.ylabel('Speed (km/h)')  # изменено на километры в час
+    plt.title('Speed vs Distance\nAverage Speed: {:.2f} km/h'.format(average_speed))  # добавление средней скорости в заголовок
     plt.grid(True)
     plt.savefig('speed_graph.png')
     plt.show()
 
 # Путь к GPX файлу
-gpx_file_path = 'mushroms_2023-05-24_15-59_Wed.gpx'
+gpx_file_path = 'your_track.gpx'
 
 # Чтение GPX файла
 points = read_gpx_file(gpx_file_path)
@@ -67,4 +73,3 @@ visualize_route(points)
 
 # Построение графика скорости
 plot_speed_graph(points)
-
